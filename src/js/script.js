3f226f361b2b4b135fed59311ba252477616d535
nobii <fujisawa-shin@kayac.com>
2018-01-05 11:06:49 +0900

    認証サンプル

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,22 @@ const sample = new Sample({
 $('.wrapper').on('click', () => {
     console.log(`hello, ${sample.name}.`);
 });
+
+
+document.querySelector('.js-google-login').addEventListener('click', (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+});
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        console.log('認証成功')
+        console.log(user.uid);
+    }
+});
+
+firebase.auth().getRedirectResult().catch((err) => {
+    console.log('認証失敗');
+    console.error(err);
+});
