92548926647287a562438cfc3b83b80edce3872b
nobii <fujisawa-shin@kayac.com>
2018-01-28 00:45:03 +0900

    add lint.

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true
+        }
+    },
+    "plugins": [
+        "html"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "comma-dangle": [
+            "error",
+            "always-multiline"
+        ],
+        "space-before-function-paren": [
+            "error",
+            "never"
+        ],
+    },
+    "globals": {
+        firebase: true,
+        process: true,
+        atom: true
+    }
+};
