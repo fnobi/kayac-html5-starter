4182e61cbccf506ae19ee4a2294c1792cc3d3b3d
nobii <fujisawa-shin@kayac.com>
2018-04-13 00:56:53 +0900

    store setup

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import App from '@/components/App.vue';
 import router from '@/router';
+import store from '@/store';
 
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
-    // store,
+    store,
     components: { App },
     template: '<App/>',
 });
