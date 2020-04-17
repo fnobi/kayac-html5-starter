3714b81a33c9805608f4ccf63ad25f2d61fb6de9
nobii <fujisawa-shin@kayac.com>
2020-04-17 23:56:40 +0900

    スタイル例が謎なので変える

--- src/components/PageGame.tsx
+++ src/components/PageGame.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { css } from "@emotion/core";
 import { px, percent } from "~/js/lib/cssUtil";
 
-const keyboardStyle = css({
+const wrapperStyle = css({
   position: "fixed",
-  bottom: px(0),
-  height: px(300),
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  top: px(0),
+  left: px(0),
   width: percent(100),
-  backgroundColor: "#0f0"
+  height: percent(100)
 });
 
 export default () => {
   return (
-    <div>
-      game page.
-      <div css={keyboardStyle} />
+    <div css={wrapperStyle}>
+      <div>game page.</div>
     </div>
   );
 };
